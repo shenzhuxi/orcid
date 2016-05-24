96d59d8dc9e741885a7ac641771aec6e8da32353
Liang <shenzhuxi@gmail.com>
2016-05-24 10:30:17 +0100

    reformat codes for Drupal standard

--- src/Controller/OauthController.php
+++ src/Controller/OauthController.php
@@ -7,124 +7,126 @@ use Drupal\user\Entity\User;
 use Drupal\Core\Database\Database;
 use Drupal\Core\Controller\ControllerBase;
 
-class OauthController extends ControllerBase
-{
-    public function finish($text = '') {
-        $destination = $_SESSION['orcid']['destination'];
-        if (isset($destination)) {
-            $response = new TrustedRedirectResponse($destination);
-            drupal_set_message(t($text));
-            unset($_SESSION['orcid']['destination']);
-            return $response;
-        }
-        $element = array(
-            '#markup' => t($text),
-        );
-        return $element;
+class OauthController extends ControllerBase {
+  public function finish($text = '') {
+    $destination = $_SESSION['orcid']['destination'];
+    if (isset($destination)) {
+      $response = new TrustedRedirectResponse($destination);
+      drupal_set_message(t($text));
+      unset($_SESSION['orcid']['destination']);
+      return $response;
+    }
+    $element = array(
+      '#markup' => t($text),
+    );
+    return $element;
+  }
+
+  public function redirectPage() {
+    if (isset($_GET['destination'])) {
+      $_SESSION['orcid']['destination'] = $_GET['destination'];
     }
 
-    public function redirectPage() {
-        if (isset($_GET['destination'])) {
-            $_SESSION['orcid']['destination'] = $_GET['destination'];
-        }
+    $config = \Drupal::config('orcid.settings');
+    //http://members.orcid.org/api/tokens-through-3-legged-oauth-authorization
+    //Public API only at this moment
+    $provider = new \League\OAuth2\Client\Provider\GenericProvider([
+      'clientId' => $config->get('client_id'),
+      // The client ID assigned to you by the provider
+      'clientSecret' => $config->get('client_secret'),
+      // The client password assigned to you by the provider
+      'redirectUri' => Url::fromUri('base:/orcid/oauth', array('absolute' => TRUE))
+        ->toString(),
+      'urlAuthorize' => 'https://orcid.org/oauth/authorize',
+      'urlAccessToken' => 'https://pub.orcid.org/oauth/token',
+      'urlResourceOwnerDetails' => 'http://pub.orcid.org/v1.2'
+    ]);
 
-        $config = \Drupal::config('orcid.settings');
-        //http://members.orcid.org/api/tokens-through-3-legged-oauth-authorization
-        //Public API only at this moment
-        $provider = new \League\OAuth2\Client\Provider\GenericProvider([
-            'clientId'                => $config->get('client_id'),    // The client ID assigned to you by the provider
-            'clientSecret'            => $config->get('client_secret'),   // The client password assigned to you by the provider
-            'redirectUri'             => Url::fromUri('base:/orcid/oauth', array('absolute' => TRUE))->toString(),
-            'urlAuthorize'            => 'https://orcid.org/oauth/authorize',
-            'urlAccessToken'          => 'https://pub.orcid.org/oauth/token',
-            'urlResourceOwnerDetails' => 'http://pub.orcid.org/v1.2'
-        ]);
-
-        if (!isset($_GET['code'])) {
-            $options = [
-                'scope' => ['/authenticate']
-            ];
-            $authorizationUrl = $provider->getAuthorizationUrl($options);
+    if (!isset($_GET['code'])) {
+      $options = [
+        'scope' => ['/authenticate']
+      ];
+      $authorizationUrl = $provider->getAuthorizationUrl($options);
 //            $_SESSION['orchid']['state'] = $provider->getState();
-            $response = new TrustedRedirectResponse($authorizationUrl);
-            return $response;
-            //header('Location: ' . $authorizationUrl);
-            //exit;
-        }
-/*        elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['orchid']['state'])) {
-            unset($_SESSION['orchid']['state']);
-        }*/
-        try {
-            $accessToken = $provider->getAccessToken('authorization_code', [
-                'code' => $_GET['code']
-            ]);
-
-            $token = $accessToken->getToken();
-//            $_SESSION['orchid']['token'] = $token;
-            $values = $accessToken->getValues();
-            $account = \Drupal::currentUser()->getAccount();
-
-            $query = Database::getConnection()
-                ->select('orcid', 'o')
-                ->fields('o', array('uid'))
-                ->condition('orcid', $values['orcid'], '=');
-            $result = $query->execute();
-
-            foreach ($result as $item) {
-                //ORCID in record
-                $uid = $item->uid;
-                //anonymous user
-                if ($account->id() == 0) {
-                    if ($user = User::load($uid)) {
-                        user_login_finalize($user);
-                        return $this->finish('You have Logged in with ORCID!');
-                    }
-                }
-
-                if ($account->id() == $uid) {//ORCID match UID
-                    return $this->finish('Your ORCID has been connected!');
-                } else {
-                    //TODO: What if user account can't match ORCID record
-                }
-            }
-            //Existing User
-            if ($account->id()) {
-                $query = Database::getConnection()
-                    ->insert('orcid')
-                    ->fields(array('orcid' => $values['orcid'], 'uid' => $account->id()))
-                    ->execute();
-                return $this->finish('Your ORCID has been connected!');
-            }
-            //New user with New ORCID
-            if ($account->id() == 0) {
-                $new_user = array(
-                    'name' => $values['orcid'] . '@' . $token,
-                    'mail' => '',
-                    'pass' => $token,
-                    'status' => 1,
-                );
-                if ($config->get('name_field')) {
-                    $new_user[$config->get('name_field')] = $values['name'];
-                }
-                $user = User::create($new_user);
-                $user->save();
-                $query = Database::getConnection()
-                    ->insert('orcid')
-                    ->fields(array('orcid' => $values['orcid'], 'uid' => $user->id()))
-                    ->execute();
-                user_login_finalize($user);
-                return $this->finish('Your account has been created with your ORCID!');
-            }
-            /*
-            $message .= $accessToken->getRefreshToken() . "\n";
-            $message .= $accessToken->getExpires() . "\n";
-            $message .= ($accessToken->hasExpired() ? 'expired' : 'not expired') . "\n";
-            */
-
-        } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
-            \Drupal::logger('orcid')->error($e->getMessage());
-            //exit($e->getMessage());
-        }
-        return $this->finish('Failed!');
+      $response = new TrustedRedirectResponse($authorizationUrl);
+      return $response;
+      //header('Location: ' . $authorizationUrl);
+      //exit;
     }
+    /*        if (isset($_SESSION['orchid']['token'])) {
+                if ($provider->getAccessToken('expires_in')->getExpires()) {
+                    $accessToken->getRefreshToken();
+                }
+            }*/
+    /*        elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['orchid']['state'])) {
+                unset($_SESSION['orchid']['state']);
+            }*/
+    try {
+      $accessToken = $provider->getAccessToken('authorization_code', [
+        'code' => $_GET['code']
+      ]);
+
+      $token = $accessToken->getToken();
+      $_SESSION['orchid']['token'] = $token;
+      $values = $accessToken->getValues();
+      $account = \Drupal::currentUser()->getAccount();
+
+      $query = Database::getConnection()
+        ->select('orcid', 'o')
+        ->fields('o', array('uid'))
+        ->condition('orcid', $values['orcid'], '=');
+      $result = $query->execute();
+
+      foreach ($result as $item) {
+        //ORCID in record
+        $uid = $item->uid;
+        //anonymous user
+        if ($account->id() == 0) {
+          if ($user = User::load($uid)) {
+            user_login_finalize($user);
+            return $this->finish('You have Logged in with ORCID!');
+          }
+        }
+
+        if ($account->id() == $uid) {//ORCID match UID
+          return $this->finish('Your ORCID has been connected!');
+        }
+        else {
+          //TODO: What if user account can't match ORCID record
+        }
+      }
+      //Existing User
+      if ($account->id()) {
+        $query = Database::getConnection()
+          ->insert('orcid')
+          ->fields(array('orcid' => $values['orcid'], 'uid' => $account->id()))
+          ->execute();
+        return $this->finish('Your ORCID has been connected!');
+      }
+      //New user with New ORCID
+      if ($account->id() == 0) {
+        $new_user = array(
+          'name' => $values['orcid'] . '@' . $token,
+          'mail' => '',
+          'pass' => $token,
+          'status' => 1,
+        );
+        if ($config->get('name_field')) {
+          $new_user[$config->get('name_field')] = $values['name'];
+        }
+        $user = User::create($new_user);
+        $user->save();
+        $query = Database::getConnection()
+          ->insert('orcid')
+          ->fields(array('orcid' => $values['orcid'], 'uid' => $user->id()))
+          ->execute();
+        user_login_finalize($user);
+        return $this->finish('Your account has been created with your ORCID!');
+      }
+    } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+      \Drupal::logger('orcid')->error($e->getMessage());
+      //exit($e->getMessage());
+    }
+    return $this->finish('Failed!');
+  }
 }
--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -3,69 +3,57 @@ namespace Drupal\orcid\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal;
 
 class SettingsForm extends ConfigFormBase {
-    public function getFormId() {
-        return 'orcid_admin_settings';
+  public function getFormId() {
+    return 'orcid_admin_settings';
+  }
+
+  protected function getEditableConfigNames() {
+    return [
+      'orcid.settings',
+    ];
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('orcid.settings');
+    $form['client_id'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Client ID'),
+      '#default_value' => $config->get('client_id'),
+      '#description' => t('The client id value <client-id> from ORCID client application registration')
+    );
+    $form['client_secret'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Client secret'),
+      '#default_value' => $config->get('client_secret'),
+      '#description' => t('The client secret value <client-secret> from ORCID client application registration'),
+    );
+    $entityManager = \Drupal::service('entity_field.manager');
+    $fields = $entityManager->getFieldDefinitions('user', 'user');
+    $user_fields = array();
+    foreach ($fields as $key => $field) {
+      if (($field->getType() == 'string') && strpos($key, 'field_') === 0) {
+        $user_fields[$key] = t($field->getLabel());
+      }
     }
-    protected function getEditableConfigNames()
-    {
-        return [
-            'orcid.settings',
-        ];
-    }
-    public function buildForm(array $form, FormStateInterface $form_state) {
-        $config = $this->config('orcid.settings');
-        $form['client_id'] = array(
-            '#type' => 'textfield',
-            '#title' => t('Client ID'),
-            '#default_value' => $config->get('client_id'),
-            '#description' => t('The client id value <client-id> from ORCID client application registration')
-        );
-        $form['client_secret'] = array(
-            '#type' => 'textfield',
-            '#title' => t('Client secret'),
-            '#default_value' => $config->get('client_secret'),
-            '#description' => t('The client secret value <client-secret> from ORCID client application registration'),
-        );
-        $entityManager = Drupal::service('entity_field.manager');
-        $fields = $entityManager->getFieldDefinitions('user', 'user');
-        $user_fields = array();
-        foreach($fields as $key => $field) {
-            if (($field->getType() == 'string') && strpos($key, 'field_') === 0) {
-                $user_fields[$key] = t($field->getLabel());
-            }
-        }
-/*        $form['orcid_field'] = array(
-            '#type' => 'select',
-            '#options' => $user_fields,
-            '#title' => t('User field for ORCID'),
-            '#default_value' => $config->get('orcid_field'),
-            '#description' => t('This field will be used to store ORCID.'),
-        );*/
-        $form['name_field'] = array(
-            '#type' => 'select',
-            '#options' => $user_fields,
-            '#empty_option' => t('- Select -'),
-            '#title' => t('User field for ORCID account name'),
-            '#default_value' => $config->get('name_field'),
-            '#description' => t('This field will be used to store the ORCID author name.'),
-        );
-        return parent::buildForm($form, $form_state);
-    }
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-        $values = $form_state->getValues();
-        $this->config('orcid.settings')
-            ->set('client_id', $values['client_id'])
-            ->set('client_secret', $values['client_secret'])
-            //->set('orcid_field', $values['orcid_field'])
-            ->set('name_field', $values['name_field'])
-            ->save();
-    }
-/*    public function validateForm(array &$form, FormStateInterface $form_state) {
-        if ($form_state->getValue('orcid_field') ==  $form_state->getValue('name_field')) {
-            $form_state->setErrorByName('orcid', $this->t(''));
-        }
-    }*/
+    $form['name_field'] = array(
+      '#type' => 'select',
+      '#options' => $user_fields,
+      '#empty_option' => t('- Select -'),
+      '#title' => t('User field for ORCID account name'),
+      '#default_value' => $config->get('name_field'),
+      '#description' => t('This field will be used to store the ORCID author name.'),
+    );
+    return parent::buildForm($form, $form_state);
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $this->config('orcid.settings')
+      ->set('client_id', $values['client_id'])
+      ->set('client_secret', $values['client_secret'])
+      ->set('name_field', $values['name_field'])
+      ->save();
+  }
 }
